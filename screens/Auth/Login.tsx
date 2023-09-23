d3bf81b440796ad9e0053fd19278f810a5f70dac
Uğur Atakan SÜRMELİ <uguratakansurmeli@gmail.com>
2023-09-24 01:48:16 +0300

    auth

---
+++ screens/Auth/Login.tsx
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {loginSuccess} from '../../utils/redux/reducers/userReducer';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    dispatch(
+      loginSuccess({
+        user: {
+          name: 'Fatih',
+          lastName: 'Ateş',
+        },
+        accessToken: 'access-token',
+      }),
+    );
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logo}>Your Logo</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={text => setEmail(text)}
+          value={email}
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    marginBottom: 30,
+  },
+  logo: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    marginBottom: 20,
+  },
+  loginButton: {
+    backgroundColor: '#007BFF',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
