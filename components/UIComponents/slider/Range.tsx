d847c780faee6f9b8a8e3271cee5fdae311df86e
Uğur Atakan SÜRMELİ <uguratakansurmeli@gmail.com>
2023-10-04 21:15:29 +0300

    .

--- components/UIComponents/slider/Range.tsx
+++ components/UIComponents/slider/Range.tsx
@@ -3,20 +3,18 @@ import { useDispatch } from 'react-redux';
 import { setVolume } from "../../../utils/redux/reducers/gameReducers/soundVolume";
 
 export default function Range() {
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   return (
     <Slider
-      style={{width: 250, height: 60}}
+      style={{ width: 250, height: 60 }}
       minimumValue={0}
       maximumValue={1}
       value={1}
       minimumTrackTintColor="#FFFFFF"
       maximumTrackTintColor="#000000"
-      onValueChange={(value)=> {
-        console.log('value changed');
-        dispatch(setVolume(value*0.05))
-      }}
+      onValueChange={(value) => dispatch(setVolume(value * 0.05))
+      }
     />
-  
+
   );
 }
