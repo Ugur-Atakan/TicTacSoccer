7b7f5aad1dc3efc8c9c78225e4198947fb4dddbc
Ugur-Atakan <uguratakansurmeli@gmail.com>
2023-11-25 01:15:52 +0300

    Refactor game action thunks and fix formatting issues

--- utils/redux/actions/game.ts
+++ utils/redux/actions/game.ts
@@ -1,25 +1,20 @@
 import baseAPI from "../../http/base";
 import { goNextRound, nextPlayer, play, resetGame, setTeamCells, started, sycnGame } from "../reducers/game.duck";
-import  {socket} from "../../SocketService";
+import { socket } from "../../SocketService";
 
-//SOCKET EMITTER THUNKS
-
-export const playOnline = (roomCode:string,userId:any, payload: any) => async (dispatch: any) => {
+export const playOnline = (roomCode: string, userId: any, payload: any) => async (dispatch: any) => {
     await dispatch(play(payload));
-    socket?.emit('play', { roomCode,userId, data:payload});
-    console.log('Play online logu',{roomCode, userId, data:payload})
-
+    socket?.emit('play', { roomCode, userId, data: payload });
 };
 
-export const startGame = (roomCode:string) => async (dispatch: any) => {
+export const startGame = (roomCode: string) => async (dispatch: any) => {
     try {
-        const teams = (await baseAPI.get('game')).data;
-        await dispatch(setTeamCells(teams));
-        dispatch(started())
+        const teams = (await baseAPI.get("game")).data;
+        dispatch(setTeamCells(teams));
+        dispatch(started());
         socket?.emit('prepare-game', { roomCode: roomCode, teams: teams });
-
     } catch (error) {
-        console.error('Bir şeyler yanlış gitti', error);
+        console.error("Bir şeyler yanlış gitti", error);
     }
 };
 
@@ -31,46 +26,48 @@ export const updateTeamCells = (payload: any) => async (dispatch: any) => {
     await dispatch(setTeamCells(payload));
 }
 
-export const finishGame = (roomCode:any) => (dispatch: any) => {
+export const finishGame = (roomCode: any) => (dispatch: any) => {
     dispatch(resetGame());
-    socket?.emit('finish-game', {roomCode:roomCode});
-};
-
-export const nextPlayerTurn = (roomCode:any) => async (dispatch: any) => {
-    dispatch(nextPlayer());
-    socket?.emit('next-player',{roomCode:roomCode});
+    socket?.emit("finish-game", { roomCode: roomCode });
 }
 
+export const nextPlayerTurn = (roomCode: any) => async (dispatch: any) => {
+    dispatch(nextPlayer());
+    socket?.emit("next-player", { roomCode: roomCode });
+}
 
-export const nextRound = (roomCode:any) => (dispatch: any) => {
-  
-    dispatch(goNextRound());
-    baseAPI
-        .get('game')
-        .then(res => dispatch(setTeamCells(res.data)))
-        .then(dispatch(started()))
-        .catch(() => console.error('Bir şeyler yanlış gitti'));
-    socket?.emit('next-round',{roomCode:roomCode});
+export const nextRound = (roomCode: any) => async (dispatch: any) => {
+    try {
+        dispatch(goNextRound());
+        const res = await baseAPI.get("game");
+        dispatch(setTeamCells(res.data));
+        dispatch(started());
+        socket?.emit("next-round", { roomCode: roomCode });
+    } catch (error) {
+        console.error("Bir şeyler yanlış gitti", error);
+    }
 }
 
 export const synchronizeGame = (data: any) => (dispatch: any) => {
     dispatch(sycnGame(data));
 }
 
-export const gameReset=()=>(dispatch:any)=>{
+export const gameReset = () => (dispatch: any) => {
     dispatch(resetGame());
 }
-export const playListener=(payload:any)=>(dispatch:any)=>{
+
+export const playListener = (payload: any) => (dispatch: any) => {
     dispatch(play(payload));
 }
 
-export const listenerNextPlayerTurn = () => async (dispatch: any) => {
+export const listenerNextPlayerTurn = () => (dispatch: any) => {
     dispatch(nextPlayer());
 }
-export const listenerNextRound = () => async (dispatch: any) => {
+
+export const listenerNextRound = () => (dispatch: any) => {
     dispatch(goNextRound());
 }
-export const listenerFinishGame = () => async (dispatch: any) => {
+
+export const listenerFinishGame = () => (dispatch: any) => {
     dispatch(resetGame());
 }
-/// Path: utils/redux/actions/game.ts
